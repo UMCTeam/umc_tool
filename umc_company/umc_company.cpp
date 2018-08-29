c020f6a4fb4ed39c04eeb00af645410536370e71
chendaole <chendaole@4399inc.com>
2018-08-29 09:18:33 +0800

    初始化工程

---
+++ umc_company/umc_company.cpp
@@ -0,0 +1,9 @@
+#include "umc_company.h" 
+
+#include <iostream>
+
+using namespace std;
+
+void hello() {
+	cout << "hello" << endl;
+}
---
+++ umc_tool/umc_tool.cpp
@@ -0,0 +1,14 @@
+﻿ // umc_tool.cpp: 定义应用程序的入口点。
+//
+
+#include "umc_tool.h"
+#include "umc_company.h"
+
+using namespace std;
+
+int main()
+{
+	hello();
+	cout << "Hello CMake。" << endl;
+	return 0;
+}
